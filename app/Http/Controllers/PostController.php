2941e85ba39cacf6a63b895bf1562edfd28fea19
guilherme <devguilhermeribeiro000@gmail.com>
2024-09-07 18:15:42 -0300

    Creating the PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  public function index()
+  {
+    $posts = Post::all();
+    return view('post.index', compact('posts'));
+  }
+  
+  public function create()
+  {
+    return view('post.create');
+  }
+  
+  public function store(Request $request)
+  {
+    $request->validate([
+      'title' => 'required|string|max:255',
+      'content' => 'required'
+    ]);
+    
+    Post::create($request->all());
+    
+    return redirect()->route('post.index');
+  }
+  
+  public function edit(Post $post)
+  {
+    return view('post.edit', compact('post'));
+  }
+  
+  public function update(Request $request, Post $post)
+  {
+    $request->validate([
+      'title' => 'required|string|max:255',
+      'content' => 'required'
+    ]);
+    
+    $post.update($request->all());
+    
+    return redirect()->route('posts.index');
+  }
+  
+  public function destroy(Post $post)
+  {
+    $post->delete();
+    return redirect()->route('posts.index');
+  }
+}
